9269584079bf5b43c5671fa0031f2f1567004766
jonapachanga <jonatan.eseiza@gmail.com>
2018-04-18 17:05:08 -0300

    strict mode on (#1)
    * strict mode on
    
    * cambios
    
    * se revirtieron cambios de test

--- src/Multinexo/Afip/WSFE/WsParametros.php
+++ src/Multinexo/Afip/WSFE/WsParametros.php
@@ -7,6 +7,8 @@
  * For the full copyright and license information, please view the LICENSE
  */
 
+declare(strict_types=1);
+
 namespace Multinexo\Afip\WSFE;
 
 class WsParametros
--- src/config/config.php
+++ src/config/config.php
@@ -6,6 +6,9 @@
  *
  * For the full copyright and license information, please view the LICENSE
  */
+
+declare(strict_types=1);
+
 $configuracion = [
     /*
     |--------------------------------------------------------------------------
