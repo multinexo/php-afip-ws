ff4f4ed10e673fb8e5167244d52befb05ba18bef
pablorsk <pablo@reyesoft.com>
2022-04-29 15:10:25 -0300

    fix/issue-41-CbteAsoc auto commit

--- src/Objects/InvoiceObject.php
+++ src/Objects/InvoiceObject.php
@@ -3,7 +3,6 @@
 namespace Multinexo\Objects;
 
 /**
- * @property int $cantidadRegistros
  * @property int $puntoVenta
  * @property int $codigoComprobante
  * @property int $codigoConcepto
@@ -12,12 +11,25 @@ namespace Multinexo\Objects;
  * @property int $numeroComprobante // todo: depende de la cantidad de fact enviadas
  * @property string $fechaEmision
  * @property int $importeTotal
+ * @property int $importeGravado
  * @property int $importeNoGravado
  * @property int $importeExento
  * @property int $importeIVA
- * @property int $codigoMoneda
- * @property int $cotizacionMoneda
  */
 class InvoiceObject extends \stdClass
 {
+    /** @var int */
+    public  $cantidadRegistros = 1;
+    /** @var string */
+    public  $codigoMoneda = 'PES';
+    /** @var int */
+    public  $cotizacionMoneda = 1;
+    /** @var int */
+    public  $fechaServicioDesde = null;
+    /** @var int */
+    public  $fechaServicioHasta = null;
+    /** @var int */
+    public  $fechaVencimientoPago = null;
+    /** @var array|null */
+    public  $arrayComprobantesAsociados = null;
 }
