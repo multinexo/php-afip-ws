d18cd8e0aa70684d151012d330cca5607e769bff
Pablo Reyes <pablo@reyesoft.com>
2025-04-28 16:05:10 -0300

    fix/observations-msg auto-commit

--- src/Objects/Invoices/ConceptEnum.php
+++ src/Objects/Invoices/ConceptEnum.php
@@ -5,7 +5,7 @@ namespace Multinexo\Objects\Invoices;
 /**
  * https://www.arca.gob.ar/ws/documentacion/manuales/manual-desarrollador-ARCA-COMPG-v4-0.pdf
  */
-class ConceptoEnum
+class ConceptEnum
 {
     public const PRODUCTOS = 1;
     public const SERVICIOS = 2;
